91df7c6cac98c1dc47540683d73f102a3d73a664
Joe Oak <joeoak@github.com>
2023-12-21 11:30:00 -0500

    Parse all XML files in data/

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
+import os
 import xml.etree.ElementTree as ET
 
-def parse_xml(file_name):
+def parse_xml(file_name, tag_name):
     # Parse XML file
     tree = ET.parse(file_name)
 
@@ -10,7 +11,14 @@ def parse_xml(file_name):
     # Print all elements in the XML file
     for elem in root.iter():
         tag = elem.tag.split('}')[-1]  # This will remove the namespace
-        print(f'{tag}: {elem.text}')
+        if tag == tag_name:
+            print(f'- {tag}: {elem.text}')
+
+def parse_all_xml(directory, tag_name):
+    for filename in os.listdir(directory):
+        if filename.endswith('.xml'):
+            print(f'{filename}')
+            parse_xml(os.path.join(directory, filename), tag_name)
 
 if __name__ == "__main__":
-    parse_xml('data/file.xml')
+    parse_all_xml('data', 'TotalTaxAmt')
